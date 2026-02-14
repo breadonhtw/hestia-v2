e530301743636aa42b0997be57de2605d6d32765
breadonhtw <e1354394@u.nus.edu>
2026-02-14 14:05:59 +0800

    Added Login Page with email-based authentication using Supabase. The page features a responsive design with a hero image and a simple form for users to enter their email address to receive a magic link for authentication. The authentication logic is handled in the `authStore`, which interacts with Supabase to send the magic link and manage user sessions.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "/Users/brandon/Desktop/hestia-v2/src/lib/database.types.ts";
+import type { Database } from "./database.types";
 
 const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
